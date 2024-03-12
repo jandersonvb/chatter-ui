47e0964bd4d9ec32e24528c8589c2f01e3a40c5f
jandersonvb <jandersonvb.dev@gmail.com>
2024-03-12 16:44:58 -0300

    feat: create hook useLogin

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -1,10 +1,19 @@
 import { Link } from "react-router-dom"
 import { Link as MUILink } from "@mui/material"
 import { Auth } from "./Auth"
+import { useLogin } from "../../hooks/useLogin"
 
 export const Login = () => {
+  const { login, error } = useLogin()
+
+  const onSubmit = async (email: string, password: string) => {
+    await login({ email, password })
+  }
+
+
   return (
-    <Auth submitLabel="Entrar" onSubmit={async () => { }}>
+    <Auth submitLabel="Entrar" onSubmit={onSubmit}
+    >
       <Link to={"/signup"} style={{ alignSelf: "center" }}>
         <MUILink style={{ alignSelf: "center" }}>Criar nova conta</MUILink>
       </Link>
