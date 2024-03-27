3df7c6114f8268a11d084bb62ff72143e72c3074
jandersonvb <jandersonvb.dev@gmail.com>
2024-03-27 15:10:38 -0300

    feat: create logout in util folder

--- src/utils/logout.ts
+++ src/utils/logout.ts
@@ -4,7 +4,5 @@ import { client } from "../constants/apollo-client";
 export const onLogout = () => {
   router.navigate('/login');
   client.resetStore();
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
   client.onResetStore(() => new Promise(() => { }));
-
 }
